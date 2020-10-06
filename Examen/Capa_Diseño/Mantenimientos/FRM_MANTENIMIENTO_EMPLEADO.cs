fc5ab49ab8af4669203f4c048d6f18464529437a
jmoratayat <jmoratayat2@miumg.edu.gt>
2020-10-06 13:35:05 -0600

    mantenimientos finalizados

---
+++ Examen/Capa_Diseño/Mantenimientos/FRM_MANTENIMIENTO_EMPLEADO.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Capa_Logica;
+using Capa_Diseño.Vistas;
+
+namespace Capa_Diseño.Mantenimientos
+{
+    public partial class FRM_MANTENIMIENTO_EMPLEADO : Form
+    {
+        Logica logi = new Logica();
+        public FRM_MANTENIMIENTO_EMPLEADO()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FRM_EMPLEADO frpro = new FRM_EMPLEADO();
+            frpro.ShowDialog();
+            if (frpro.DialogResult == DialogResult.OK)
+            {
+                textBox1.Text = frpro.dataGridView1.Rows[frpro.dataGridView1.CurrentRow.Index].Cells[0].Value.ToString(); //para jalar el dato de datagridview
+                textBox2.Text = frpro.dataGridView1.Rows[frpro.dataGridView1.CurrentRow.Index].Cells[1].Value.ToString(); //valor 2
+                textBox3.Text = frpro.dataGridView1.Rows[frpro.dataGridView1.CurrentRow.Index].Cells[2].Value.ToString(); //valor 3
+                textBox4.Text = frpro.dataGridView1.Rows[frpro.dataGridView1.CurrentRow.Index].Cells[3].Value.ToString(); //valor 4
+                textBox5.Text = frpro.dataGridView1.Rows[frpro.dataGridView1.CurrentRow.Index].Cells[4].Value.ToString(); //valor 5
+                textBox6.Text = frpro.dataGridView1.Rows[frpro.dataGridView1.CurrentRow.Index].Cells[5].Value.ToString(); //valor 6
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FRM_MANTENIMIENTO_PROVEEDOR_Load(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            textBox5.Enabled = false;
+            textBox6.Enabled = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
+            textBox5.Enabled = true;
+            textBox6.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] valores = { textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text}; //valores a ingresar
+            if (logi.insertar_empleado(valores) == null)
+            {
+                MessageBox.Show("Error al guardar");
+            }
+            else
+            {
+                MessageBox.Show("Guardado exitosamente");
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                textBox5.Enabled = false;
+                textBox6.Enabled = false;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] valores = { textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text }; //valores a ingresar
+            if (logi.modificar_empleado(valores) == null)
+            {
+                MessageBox.Show("Error al modificar");
+            }
+            else
+            {
+                MessageBox.Show("Modificacion exitosa");
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                textBox5.Enabled = false;
+                textBox6.Enabled = false;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] valores = { textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text}; //valores a ingresar
+            if (logi.eliminar_empleado(valores) == null)
+            {
+                MessageBox.Show("Error al eliminar");
+            }
+            else
+            {
+                MessageBox.Show("eliminacion exitosa");
+               
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                textBox5.Enabled = false;
+                textBox6.Enabled = false;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+
+            }
+
+        }
+    }
+}
---
+++ Examen/Capa_Diseño/Vistas/FRM_EMPLEADO.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Capa_Diseño.Vistas
+{
+    partial class FRM_EMPLEADO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_actualizar = new System.Windows.Forms.Button();
+            this.btn_seleccionar = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cod_puesto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cod_departamento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sueldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.nombre,
+            this.cod_puesto,
+            this.cod_departamento,
+            this.sueldo,
+            this.estado});
+            this.dataGridView1.Location = new System.Drawing.Point(33, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(969, 275);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btn_actualizar
+            // 
+            this.btn_actualizar.Location = new System.Drawing.Point(69, 94);
+            this.btn_actualizar.Name = "btn_actualizar";
+            this.btn_actualizar.Size = new System.Drawing.Size(116, 23);
+            this.btn_actualizar.TabIndex = 1;
+            this.btn_actualizar.Text = "Actualizar";
+            this.btn_actualizar.UseVisualStyleBackColor = true;
+            this.btn_actualizar.Click += new System.EventHandler(this.btn_actualizar_Click);
+            // 
+            // btn_seleccionar
+            // 
+            this.btn_seleccionar.Location = new System.Drawing.Point(694, 94);
+            this.btn_seleccionar.Name = "btn_seleccionar";
+            this.btn_seleccionar.Size = new System.Drawing.Size(108, 23);
+            this.btn_seleccionar.TabIndex = 2;
+            this.btn_seleccionar.Text = "Seleccionar";
+            this.btn_seleccionar.UseVisualStyleBackColor = true;
+            this.btn_seleccionar.Click += new System.EventHandler(this.btn_seleccionar_Click);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.Width = 125;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "NOMBRE";
+            this.nombre.MinimumWidth = 6;
+            this.nombre.Name = "nombre";
+            this.nombre.Width = 125;
+            // 
+            // cod_puesto
+            // 
+            this.cod_puesto.HeaderText = "COD PUESTO";
+            this.cod_puesto.MinimumWidth = 6;
+            this.cod_puesto.Name = "cod_puesto";
+            this.cod_puesto.Width = 125;
+            // 
+            // cod_departamento
+            // 
+            this.cod_departamento.HeaderText = "COD DEPARTAMENTO";
+            this.cod_departamento.MinimumWidth = 6;
+            this.cod_departamento.Name = "cod_departamento";
+            this.cod_departamento.Width = 125;
+            // 
+            // sueldo
+            // 
+            this.sueldo.HeaderText = "SUELDO";
+            this.sueldo.MinimumWidth = 6;
+            this.sueldo.Name = "sueldo";
+            this.sueldo.Width = 125;
+            // 
+            // estado
+            // 
+            this.estado.HeaderText = "ESTADO";
+            this.estado.MinimumWidth = 6;
+            this.estado.Name = "estado";
+            this.estado.Width = 125;
+            // 
+            // FRM_PROVEEDOR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 536);
+            this.Controls.Add(this.btn_seleccionar);
+            this.Controls.Add(this.btn_actualizar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FRM_PROVEEDOR";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EMPLEADO";
+            this.Load += new System.EventHandler(this.FRM_PROVEEDOR_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_actualizar;
+        private System.Windows.Forms.Button btn_seleccionar;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cod_puesto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cod_departamento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sueldo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn estado;
+    }
+}
